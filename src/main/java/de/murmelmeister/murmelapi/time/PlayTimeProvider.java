2e8c7fa0a7ba4bae69486743b0818804933b4544
Murmelmeister <49274952+Murmelmeister@users.noreply.github.com>
2025-04-12 23:17:56 +0200

    Refactor PlayTimeProvider to use Database class for database operations and update method signatures

--- src/main/java/de/murmelmeister/murmelapi/time/PlayTimeProvider.java
+++ src/main/java/de/murmelmeister/murmelapi/time/PlayTimeProvider.java
@@ -1,109 +1,91 @@
 package de.murmelmeister.murmelapi.time;
 
-import de.murmelmeister.murmelapi.user.User;
-import de.murmelmeister.murmelapi.utils.Database;
 
+import de.murmelmeister.murmelapi.database.Database;
+
+/**
+ * The PlayTimeProvider class provides methods to manage playtime data in the database.
+ * It implements the PlayTime interface and uses stored procedures for database operations.
+ */
 public final class PlayTimeProvider implements PlayTime {
-    public PlayTimeProvider(User user) {
-        String tableName = "PlayTime";
-        createTable(tableName);
-        Procedure.loadAll(tableName);
-        loadTables(user);
+    private static final String TABLE_NAME = "playtime";
+
+    private final Database database;
+
+    public PlayTimeProvider(Database database) {
+        this.database = database;
     }
 
-    private void createTable(String tableName) {
-        Database.createTable(tableName, "UserID INT PRIMARY KEY, Seconds INT");
+    public static void setup(Database database) {
+        database.createTable(TABLE_NAME, "userId INT PRIMARY KEY, seconds INT DEFAULT 0, " +
+                                         "FOREIGN KEY (userId) REFERENCES users(id)");
+        Procedure.loadAll(database);
     }
 
     @Override
     public boolean existsUser(int userId) {
-        return Database.callExists(Procedure.PLAY_TIME_USER_ID.getName(), userId);
+        return userId > 0 && database.existsCallable(Procedure.GET_DATA.getName(), userId);
     }
 
     @Override
-    public void createUser(int userId) {
-        if (existsUser(userId)) return;
-        Database.callUpdate(Procedure.PLAY_TIME_INSERT.getName(), userId, 0);
+    public int createUser(int userId) {
+        if (userId < 1) return 0;
+        return database.updateCallable(Procedure.CREATE.getName(), userId);
     }
 
     @Override
-    public void deleteUser(int userId) {
-        Database.callUpdate(Procedure.PLAY_TIME_DELETE.getName(), userId);
+    public int deleteUser(int userId) {
+        if (userId < 1) return 0;
+        return database.updateCallable(Procedure.DELETE.getName(), userId);
     }
 
     @Override
     public int getTime(int userId) {
-        return Database.callQuery(-1, "Seconds", int.class, Procedure.PLAY_TIME_USER_ID.getName(), userId);
+        if (userId < 1) return -1;
+        return database.queryCallable(Procedure.GET_DATA.getName(), -1, resultSet -> resultSet.getInt("seconds"), userId);
     }
 
     @Override
-    public void setTime(int userId, int time) {
-        Database.callUpdate(Procedure.PLAY_TIME_UPDATE.getName(), userId, time);
+    public void updateTime(int userId, int seconds) {
+        if (userId < 1) return;
+        database.updateCallable(Procedure.UPDATE_SECONDS.getName(), userId, seconds);
     }
 
     @Override
     public void addTime(int userId) {
-        int current = getTime(userId);
-        ++current;
-        setTime(userId, current);
-    }
-
-    @Override
-    public void addTime(int userId, PlayTimeType type, int time) {
-        int current = getTime(userId);
-        current = current + type.toSeconds(time);
-        setTime(userId, current);
-    }
-
-    @Override
-    public void removeTime(int userId) {
-        int current = getTime(userId);
-        --current;
-        setTime(userId, current);
-    }
-
-    @Override
-    public void removeTime(int userId, PlayTimeType type, int time) {
-        int current = getTime(userId);
-        current = current - type.toSeconds(time);
-        setTime(userId, current);
-    }
-
-    @Override
-    public void resetTime(int userId) {
-        setTime(userId, 0);
-    }
-
-    private void loadTables(User user) {
-        for (int userId : user.getIds())
-            createUser(userId);
+        if (userId < 1) return;
+        int currentTime = getTime(userId);
+        ++currentTime;
+        updateTime(userId, currentTime);
     }
 
     private enum Procedure {
-        PLAY_TIME_USER_ID("PlayTime_UserID", "uid INT", "SELECT * FROM [TABLE] WHERE UserID=uid;"),
-        PLAY_TIME_INSERT("PlayTime_Insert", "uid INT, sec INT", "INSERT INTO [TABLE] VALUES (uid, sec);"),
-        PLAY_TIME_DELETE("PlayTime_Delete", "uid INT", "DELETE FROM [TABLE] WHERE UserID=uid;"),
-        PLAY_TIME_UPDATE("PlayTime_Update", "uid INT, sec INT", "UPDATE [TABLE] SET Seconds=sec WHERE UserID=uid;");
+        CREATE("playtime_create", "p_userId INT", "INSERT INTO [TABLE] (userId) VALUES (uid);"),
+        DELETE("playtime_delete", "p_userId INT", "DELETE FROM [TABLE] WHERE userId=p_userId;"),
+        GET_DATA("playtime_getData", "p_userId INT", "SELECT * FROM [TABLE] WHERE userId=p_userId;"),
+        UPDATE_SECONDS("playtime_updateSeconds", "p_userId INT, p_seconds INT",
+                "UPDATE [TABLE] SET seconds=p_seconds WHERE userId=p_userId;");
         private static final Procedure[] VALUES = values();
 
         private final String name;
         private final String query;
 
-        Procedure(final String name, final String input, final String query) {
+        Procedure(String name, String input, String query) {
             this.name = name;
-            this.query = Database.getProcedureQueryWithoutObjects(name, input, query);
+            this.query = Database.getProcedureQuery(name, input, query);
         }
 
         public String getName() {
             return name;
         }
 
-        public String getQuery(String tableName) {
-            return query.replace("[TABLE]", tableName);
+        public String getQuery() {
+            return query.replace("[TABLE]", TABLE_NAME);
         }
 
-        public static void loadAll(String tableName) {
-            for (Procedure procedure : VALUES) Database.update(procedure.getQuery(tableName));
+        public static void loadAll(Database database) {
+            for (Procedure procedure : VALUES)
+                database.update(procedure.getQuery());
         }
     }
 }
