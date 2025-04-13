28570cf15d53f860929cd294a05a09a28709816c
Murmelmeister <49274952+Murmelmeister@users.noreply.github.com>
2025-04-13 14:31:57 +0200

    Refactor GroupProvider to use Database for CRUD operations and enhance group management methods

--- src/main/java/de/murmelmeister/murmelapi/group/GroupProvider.java
+++ src/main/java/de/murmelmeister/murmelapi/group/GroupProvider.java
@@ -1,165 +1,196 @@
 package de.murmelmeister.murmelapi.group;
 
-import de.murmelmeister.murmelapi.group.parent.GroupParent;
-import de.murmelmeister.murmelapi.group.parent.GroupParentProvider;
-import de.murmelmeister.murmelapi.group.permission.GroupPermission;
-import de.murmelmeister.murmelapi.group.permission.GroupPermissionProvider;
-import de.murmelmeister.murmelapi.group.settings.*;
-import de.murmelmeister.murmelapi.utils.Database;
+import de.murmelmeister.murmelapi.database.Database;
+import de.murmelmeister.murmelapi.group.color.GroupColor;
+import de.murmelmeister.murmelapi.group.color.GroupColorProvider;
 
+import java.sql.Timestamp;
 import java.util.List;
 
-public final class GroupProvider implements Group {
-    private final GroupSettings settings;
-    private final GroupColorSettings colorSettings;
-    private final GroupParent parent;
-    private final GroupPermission permission;
+import static de.murmelmeister.murmelapi.MurmelAPI.getDateFormat;
 
-    public GroupProvider() {
-        String tableName = "Groups";
-        createTable(tableName);
-        Procedure.loadAll(tableName);
-        this.settings = new GroupSettingsProvider();
-        this.colorSettings = new GroupColorSettingsProvider();
-        this.parent = new GroupParentProvider();
-        this.permission = new GroupPermissionProvider();
-        createDefaultGroup();
+/**
+ * GroupProvider is a class that provides methods to manage groups in the database.
+ * It implements the Group interface and uses the Database class to interact with the database.
+ */
+public final class GroupProvider implements Group {
+    private static final String TABLE_NAME = "groups";
+
+    private final Database database;
+    private GroupColor color;
+
+    public GroupProvider(Database database) {
+        this.database = database;
+        this.color = getColor();
     }
 
-    private void createTable(String tableName) {
-        Database.createTable(tableName, "ID INT PRIMARY KEY AUTO_INCREMENT, GroupName VARCHAR(100)");
+    public static void setup(Database database) {
+        database.createTable(TABLE_NAME, "id INT PRIMARY KEY AUTO_INCREMENT, " +
+                                         "groupName VARCHAR(100) UNIQUE, " +
+                                         "priority INT, " +
+                                         "teamSort VARCHAR(100), " +
+                                         "createdBy INT, FOREIGN KEY (createdBy) REFERENCES users(id), " +
+                                         "createdAt DATETIME DEFAULT CURRENT_TIMESTAMP(), " +
+                                         "updatedBy INT, FOREIGN KEY (updatedBy) REFERENCES users(id), " +
+                                         "updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP()");
+        Procedure.loadAll(database);
     }
 
     @Override
     public boolean existsGroup(int id) {
-        return Database.callExists(Procedure.GROUP_ID.getName(), id);
+        return id > 0 && database.existsCallable(Procedure.GET_DATA_BY_ID.getName(), id);
     }
 
     @Override
-    public boolean existsGroup(String name) {
-        return Database.callExists(Procedure.GROUP_NAME.getName(), name);
+    public boolean existsGroup(String groupName) {
+        return groupName != null && database.existsCallable(Procedure.GET_DATA_BY_NAME.getName(), groupName);
     }
 
     @Override
-    public void createNewGroup(String name, int creatorId, int sortId, String teamId) {
-        if (existsGroup(name)) return;
-        Database.callUpdate(Procedure.GROUP_INSERT.getName(), name);
-        int id = getUniqueId(name);
-        String team = teamId + getName(id);
-        settings.createGroup(id, creatorId, sortId, team);
-        colorSettings.createGroup(id, creatorId);
+    public int createGroup(String groupName, int priority, String teamId, int createdBy) {
+        if (groupName == null || priority < 0 || teamId == null || createdBy == -2) return 0;
+        String teamSort = teamId + groupName;
+        return database.updateCallable(Procedure.CREATE.getName(), groupName, priority, teamSort, createdBy, createdBy);
     }
 
     @Override
-    public void deleteGroup(int id) {
-        permission.clearPermission(id);
-        parent.clearParent(id);
-        colorSettings.deleteGroup(id);
-        settings.deleteGroup(id);
-        Database.callUpdate(Procedure.GROUP_DELETE.getName(), id);
+    public int deleteGroup(int id) {
+        if (id < 1) return 0;
+        return database.updateCallable(Procedure.DELETE.getName(), id);
     }
 
     @Override
-    public int getUniqueId(String name) {
-        return Database.callQuery(-1, "ID", int.class, Procedure.GROUP_NAME.getName(), name);
+    public int getId(String groupName) {
+        return groupName == null ? -1 : database.queryCallable(Procedure.GET_DATA_BY_NAME.getName(), -1, resultSet -> resultSet.getInt("id"), groupName);
     }
 
     @Override
-    public String getName(int id) {
-        return Database.callQuery(null, "GroupName", String.class, Procedure.GROUP_ID.getName(), id);
+    public String getGroupName(int id) {
+        return id < 1 ? null : database.queryCallable(Procedure.GET_DATA_BY_ID.getName(), null, resultSet -> resultSet.getString("groupName"), id);
     }
 
     @Override
-    public void rename(int id, String newName) {
-        Database.callUpdate(Procedure.GROUP_RENAME_BY_ID.getName(), id, newName);
+    public int rename(int id, String newName, int updatedBy) {
+        if (id < 1 || newName == null || updatedBy == -2) return 0;
+        return database.updateCallable(Procedure.UPDATE_NAME.getName(), id, newName, updatedBy);
     }
 
     @Override
-    public void rename(String oldName, String newName) {
-        Database.callUpdate(Procedure.GROUP_RENAME_BY_NAME.getName(), oldName, newName);
+    public List<Integer> getIds() {
+        return database.queryListCallable(Procedure.GET_DATA.getName(), resultSet -> resultSet.getInt("id"));
     }
 
     @Override
-    public List<Integer> getUniqueIds() {
-        return Database.callQueryList("ID", int.class, Procedure.GROUP_ALL.getName());
+    public List<String> getGroupNames() {
+        return database.queryListCallable(Procedure.GET_DATA.getName(), resultSet -> resultSet.getString("groupName"));
     }
 
     @Override
-    public List<String> getNames() {
-        return Database.callQueryList("GroupName", String.class, Procedure.GROUP_ALL.getName());
+    public int getPriority(int id) {
+        return id < 1 ? 0 : database.queryCallable(Procedure.GET_DATA_BY_ID.getName(), 0, resultSet -> resultSet.getInt("priority"), id);
     }
 
     @Override
-    public void loadExpired() {
-        parent.loadExpired(this);
-        permission.loadExpired(this);
-    }
-
-    private int createDefaultGroup() {
-        int id = 1;
-        if (existsGroup(id)) return id;
-        String name = "default";
-        Database.callUpdate(Procedure.GROUP_INSERT.getName(), name);
-        int creatorId = -1;
-        String team = 9999 + getName(id);
-        settings.createGroup(id, creatorId, 0, team);
-        colorSettings.createGroup(id, creatorId, "<gray>", "", "", "", "", "<gray>", "", "", "7");
-        return id;
+    public int setPriority(int id, int priority, int updatedBy) {
+        if (id < 1 || priority < 0 || updatedBy == -2) return 0;
+        return database.updateCallable(Procedure.UPDATE_PRIORITY.getName(), id, priority, updatedBy);
     }
 
     @Override
-    public int getDefaultGroup() {
-        return createDefaultGroup();
+    public String getTeamSort(int id) {
+        return id < 1 ? null : database.queryCallable(Procedure.GET_DATA_BY_ID.getName(), null, resultSet -> resultSet.getString("teamSort"), id);
     }
 
     @Override
-    public GroupSettings getSettings() {
-        return settings;
+    public int setTeamSort(int id, String teamSort, int updatedBy) {
+        if (id < 1 || teamSort == null || updatedBy == -2) return 0;
+        return database.updateCallable(Procedure.UPDATE_TEAM_SORT.getName(), id, teamSort, updatedBy);
     }
 
     @Override
-    public GroupColorSettings getColorSettings() {
-        return colorSettings;
+    public int getCreatedBy(int id) {
+        return id < 1 ? -2 : database.queryCallable(Procedure.GET_DATA_BY_ID.getName(), -2, resultSet -> resultSet.getInt("createdBy"), id);
     }
 
     @Override
-    public GroupParent getParent() {
-        return parent;
+    public Timestamp getCreatedAt(int id) {
+        return id < 1 ? null : database.queryCallable(Procedure.GET_DATA_BY_ID.getName(), null, resultSet -> resultSet.getTimestamp("createdAt"), id);
     }
 
     @Override
-    public GroupPermission getPermission() {
-        return permission;
+    public String getCreatedDate(int id) {
+        Timestamp time = getCreatedAt(id);
+        return time == null ? null : getDateFormat().format(time);
+    }
+
+    @Override
+    public int getUpdatedBy(int id) {
+        return id < 1 ? -2 : database.queryCallable(Procedure.GET_DATA_BY_ID.getName(), -2, resultSet -> resultSet.getInt("updatedBy"), id);
+    }
+
+    @Override
+    public Timestamp getUpdatedAt(int id) {
+        return id < 1 ? null : database.queryCallable(Procedure.GET_DATA_BY_ID.getName(), null, resultSet -> resultSet.getTimestamp("updatedAt"), id);
+    }
+
+    @Override
+    public String getUpdatedDate(int id) {
+        Timestamp time = getUpdatedAt(id);
+        return time == null ? null : getDateFormat().format(time);
+    }
+
+    @Override
+    public void createDefaultGroup(String groupName) {
+        if (groupName == null) return;
+        if (existsGroup(groupName)) return;
+        int createdBy = -1; // Console
+        int priority = 1; // Default priority
+        String teamSort = 9999 + groupName;
+        createGroup(groupName, priority, teamSort, createdBy);
+        color.createGroup(getId(groupName), "<gray>", null, null, null, null, "<gray>", null, null, "7", createdBy);
+    }
+
+    @Override
+    public GroupColor getColor() {
+        if (color == null)
+            color = new GroupColorProvider(database);
+        return color;
     }
 
     private enum Procedure {
-        GROUP_ID("Groups_ID", "gid INT", "SELECT * FROM [TABLE] WHERE ID=gid;"),
-        GROUP_NAME("Groups_Name", "gName VARCHAR(100)", "SELECT * FROM [TABLE] WHERE GroupName=gName;"),
-        GROUP_INSERT("Groups_Insert", "gName VARCHAR(100)", "INSERT INTO [TABLE] (GroupName) VALUES (gName);"),
-        GROUP_DELETE("Groups_Delete", "gid INT", "DELETE FROM [TABLE] WHERE ID=gid;"),
-        GROUP_ALL("Groups_All", "", "SELECT * FROM [TABLE];"),
-        GROUP_RENAME_BY_ID("Groups_RenameByID", "gid INT, gName VARCHAR(100)", "UPDATE [TABLE] SET GroupName=gName WHERE ID=gid;"),
-        GROUP_RENAME_BY_NAME("Groups_RenameByName", "oldGroup VARCHAR(100), newGroup VARCHAR(100)", "UPDATE [TABLE] SET GroupName=newGroup WHERE GroupName=oldGroup;");
+        CREATE("groups_create", "p_groupName VARCHAR(100), p_priority INT, p_teamSort VARCHAR(100), p_createdBy INT, p_updatedBy INT",
+                "INSERT INTO [TABLE] (groupName, priority, teamSort, createdBy, updatedBy) VALUES (p_groupName, p_priority, p_teamSort, p_createdBy, p_updatedBy);"),
+        DELETE("groups_delete", "p_id INT", "DELETE FROM [TABLE] WHERE id=p_id;"),
+        GET_DATA_BY_ID("groups_getDataById", "p_id INT", "SELECT * FROM [TABLE] WHERE id=p_id;"),
+        GET_DATA_BY_NAME("groups_getDataByName", "p_groupName VARCHAR(100)", "SELECT * FROM [TABLE] WHERE groupName=p_groupName;"),
+        GET_DATA("groups_getData", "", "SELECT * FROM [TABLE];"),
+        UPDATE_PRIORITY("groups_updatePriority", "p_id INT, p_priority INT, p_updatedBy INT",
+                "UPDATE [TABLE] SET priority=p_priority, updatedBy=p_updatedBy WHERE id=p_id;"),
+        UPDATE_TEAM_SORT("groups_updateTeamSort", "p_id INT, p_teamSort VARCHAR(100), p_updatedBy INT",
+                "UPDATE [TABLE] SET teamSort=p_teamSort, updatedBy=p_updatedBy WHERE id=p_id;"),
+        UPDATE_NAME("groups_updateName", "p_id INT, p_groupName VARCHAR(100), p_updatedBy INT",
+                "UPDATE [TABLE] SET groupName=p_groupName, updatedBy=p_updatedBy WHERE id=p_id;");
         private static final Procedure[] VALUES = values();
 
         private final String name;
         private final String query;
 
-        Procedure(final String name, final String input, final String query) {
+        Procedure(String name, String input, String query) {
             this.name = name;
-            this.query = Database.getProcedureQueryWithoutObjects(name, input, query);
+            this.query = Database.getProcedureQuery(name, input, query);
         }
 
         public String getName() {
             return name;
         }
 
-        public String getQuery(String tableName) {
-            return query.replace("[TABLE]", tableName);
+        public String getQuery() {
+            return query.replace("[TABLE]", TABLE_NAME);
         }
 
-        public static void loadAll(String tableName) {
-            for (Procedure procedure : VALUES) Database.update(procedure.getQuery(tableName));
+        private static void loadAll(Database database) {
+            for (Procedure procedure : VALUES)
+                database.update(procedure.getQuery());
         }
     }
 }
