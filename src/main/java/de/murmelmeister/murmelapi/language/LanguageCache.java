007f27c7fdc8816ee6dea0fe4c0ef09fbf5dfad5
Murmelmeister <49274952+Murmelmeister@users.noreply.github.com>
2025-05-23 23:50:30 +0200

    Add LanguageCache class to manage in-memory caching of Language entities

---
+++ src/main/java/de/murmelmeister/murmelapi/language/LanguageCache.java
@@ -0,0 +1,27 @@
+package de.murmelmeister.murmelapi.language;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class LanguageCache {
+    private final ConcurrentHashMap<Integer, Language> cache = new ConcurrentHashMap<>();
+
+    public boolean containsKey(int id) {
+        return cache.containsKey(id);
+    }
+
+    public Language get(int id) {
+        return cache.get(id);
+    }
+
+    public void put(Language language) {
+        cache.put(language.getId(), language);
+    }
+
+    public void remove(int id) {
+        cache.remove(id);
+    }
+
+    public void clear() {
+        cache.clear();
+    }
+}
