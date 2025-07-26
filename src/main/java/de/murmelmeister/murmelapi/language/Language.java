8ce877ddf5cbbe098075f06250caf1c39e1f5330
Murmelmeister <49274952+Murmelmeister@users.noreply.github.com>
2025-07-26 20:47:15 +0200

    Refactor Language class to use record type and add withName method for name updates

--- src/main/java/de/murmelmeister/murmelapi/language/Language.java
+++ src/main/java/de/murmelmeister/murmelapi/language/Language.java
@@ -4,24 +4,8 @@ package de.murmelmeister.murmelapi.language;
  * Represents a language with an ID and a name.
  * This class is used to manage different languages in the application.
  */
-public class Language {
-    private final int id;
-    private String name;
-
-    public Language(int id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
+public record Language(int id, String name) {
+    public Language withName(String name) {
+        return new Language(id, name != null ? name : this.name);
     }
 }
