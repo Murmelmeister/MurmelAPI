190530daf5bca104519b9a5d48faf945a45d6274
Murmelmeister <49274952+Murmelmeister@users.noreply.github.com>
2025-07-26 20:46:15 +0200

    Expand RefreshType enum to include additional refresh categories for enhanced data management

--- src/main/java/de/murmelmeister/murmelapi/utils/update/RefreshType.java
+++ src/main/java/de/murmelmeister/murmelapi/utils/update/RefreshType.java
@@ -7,12 +7,40 @@ package de.murmelmeister.murmelapi.utils.update;
  * It includes options for refreshing all data, refreshing global data, and refreshing permissions.
  */
 public enum RefreshType {
-    ALL("global"),
-    GLOBAL("global"),
-    PERMISSIONS("permissions"),
+    ALL("all"),
     MESSAGES("messages"),
     LANGUAGES("languages"),
-    REASONS("reasons"),;
+    PUNISHMENT_REASONS("punishment_reasons"),
+    PUNISHMENT_LOGS("punishment_logs"),
+    PUNISHMENT_USERS("punishment_users"),
+    PUNISHMENT_IPS("punishment_ips"),
+    USERS("users"),
+    USER_PERMISSIONS("user_permissions"),
+    USER_PARENTS("user_parents"),
+    USER_LOGINS("user_logins"),
+    USER_SESSIONS("user_sessions"),
+    USER_PLAY_TIMES("user_play_times"),
+    GROUPS("groups"),
+    GROUP_COLORS("group_colors"),
+    GROUP_PERMISSIONS("group_permissions"),
+    GROUP_PARENTS("group_parents"),
+    SINGLE_USER("single_user"),
+    SINGLE_USER_LOGIN("single_user_login"),
+    SINGLE_USER_SESSION("single_user_session"),
+    SINGLE_USER_PLAY_TIME("single_user_play_time"),
+    SINGLE_PUNISHMENT_REASON("single_punishment_reason"),
+    SINGLE_PUNISHMENT_LOG("single_punishment_log"),
+    SINGLE_PUNISHMENT_USER("single_punishment_user"),
+    SINGLE_PUNISHMENT_IP("single_punishment_ip"),
+    SINGLE_USER_PERMISSION("single_user_permission"),
+    SINGLE_USER_PARENT("single_user_parent"),
+    SINGLE_GROUP_PERMISSION("single_group_permission"),
+    SINGLE_GROUP_PARENT("single_group_parent"),
+    SINGLE_MESSAGE("single_message"),
+    SINGLE_LANGUAGE("single_language"),
+    SINGLE_GROUP("single_group"),
+    SINGLE_GROUP_COLOR("single_group_color")
+    ;
     private static final RefreshType[] VALUES = values();
 
     private final String name;
