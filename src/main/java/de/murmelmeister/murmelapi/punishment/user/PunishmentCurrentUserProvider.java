6ed244c89682710c07a6729398336ea204550a85
Murmelmeister <49274952+Murmelmeister@users.noreply.github.com>
2026-01-18 14:16:13 +0100

    refactor: enforce null-safety annotations in `PunishmentCurrentUserProvider` methods

--- src/main/java/de/murmelmeister/murmelapi/punishment/user/PunishmentCurrentUserProvider.java
+++ src/main/java/de/murmelmeister/murmelapi/punishment/user/PunishmentCurrentUserProvider.java
@@ -1,18 +1,24 @@
 package de.murmelmeister.murmelapi.punishment.user;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.Unmodifiable;
+
 import java.util.List;
 import java.util.UUID;
 
 public interface PunishmentCurrentUserProvider {
     void refreshCache();
 
+    @NotNull
+    @Unmodifiable
     List<Integer> getAllPunishedUserIds(int typeId);
 
-    PunishmentCurrentUser getPunishedUser(int userId, int typeId);
+    @Nullable PunishmentCurrentUser getPunishedUser(int userId, int typeId);
 
-    PunishmentCurrentUser create(int userId, int typeId, UUID logId);
+    @Nullable PunishmentCurrentUser create(int userId, int typeId, @NotNull UUID logId);
 
     int delete(int userId, int typeId);
 
-    PunishmentCurrentUser update(int userId, int typeId, UUID logId);
+    @Nullable PunishmentCurrentUser update(int userId, int typeId, @NotNull UUID logId);
 }
