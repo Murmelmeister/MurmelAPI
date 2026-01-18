0bb32d5b38a31269c314477a81c620a7f0e3949a
Murmelmeister <49274952+Murmelmeister@users.noreply.github.com>
2026-01-18 14:15:54 +0100

    refactor: add `PunishmentCurrentUser.Builder` for mutable operations and enforce null-safety

--- src/main/java/de/murmelmeister/murmelapi/punishment/user/PunishmentCurrentUser.java
+++ src/main/java/de/murmelmeister/murmelapi/punishment/user/PunishmentCurrentUser.java
@@ -1,10 +1,42 @@
 package de.murmelmeister.murmelapi.punishment.user;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
 import java.util.UUID;
 
-public record PunishmentCurrentUser(int userId, int typeId, UUID logId) {
+public record PunishmentCurrentUser(
+        int userId,
+        int typeId,
+        @NotNull UUID logId
+) {
+    public PunishmentCurrentUser {
+        Objects.requireNonNull(logId, "Log ID cannot be null");
+    }
 
-    public PunishmentCurrentUser withUpdateLog(UUID logId) {
-        return new PunishmentCurrentUser(userId, typeId, logId != null ? logId : this.logId);
+    public static @NotNull Builder builder(@NotNull PunishmentCurrentUser currentUser) {
+        return new Builder(currentUser);
+    }
+
+    public static class Builder {
+        private final int userId;
+        private final int typeId;
+
+        private UUID logId;
+
+        private Builder(@NotNull PunishmentCurrentUser currentUser) {
+            this.userId = currentUser.userId();
+            this.typeId = currentUser.typeId();
+            this.logId = currentUser.logId();
+        }
+
+        public Builder logId(@NotNull UUID logId) {
+            this.logId = logId;
+            return this;
+        }
+
+        public @NotNull PunishmentCurrentUser build() {
+            return new PunishmentCurrentUser(userId, typeId, logId);
+        }
     }
 }
