cc0a179c317e28499e3903e7503183c0f137afac
Murmelmeister <49274952+Murmelmeister@users.noreply.github.com>
2026-01-18 12:34:42 +0100

    refactor: enforce null-safety for `InventoryType` record constructor

--- src/main/java/de/murmelmeister/murmelapi/inventory/InventoryType.java
+++ src/main/java/de/murmelmeister/murmelapi/inventory/InventoryType.java
@@ -2,5 +2,10 @@ package de.murmelmeister.murmelapi.inventory;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Objects;
+
 public record InventoryType(int id, @NotNull String name) {
+    public InventoryType {
+        Objects.requireNonNull(name, "name cannot be null");
+    }
 }
