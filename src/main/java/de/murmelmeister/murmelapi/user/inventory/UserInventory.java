4fa259b3d73b67e0bd904ff79e58c5c63894fb98
Murmelmeister <49274952+Murmelmeister@users.noreply.github.com>
2026-01-18 12:38:12 +0100

    refactor: enforce null-safety for `UserInventory` record

--- src/main/java/de/murmelmeister/murmelapi/user/inventory/UserInventory.java
+++ src/main/java/de/murmelmeister/murmelapi/user/inventory/UserInventory.java
@@ -2,7 +2,17 @@ package de.murmelmeister.murmelapi.user.inventory;
 
 import org.jetbrains.annotations.NotNull;
 
-public record UserInventory(int userId, int inventoryId, @NotNull String value) {
+import java.util.Objects;
+
+public record UserInventory(
+        int userId,
+        int inventoryId,
+        @NotNull String value
+) {
+    public UserInventory {
+        Objects.requireNonNull(value, "value cannot be null");
+    }
+
     public UserInventory withValue(String value) {
         return new UserInventory(userId, inventoryId, value != null ? value : this.value);
     }
