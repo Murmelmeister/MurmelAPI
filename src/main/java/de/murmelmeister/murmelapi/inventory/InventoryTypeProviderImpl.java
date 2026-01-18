ecfde015d833a3bacc7f0f1b20cf5718e0a1736b
Murmelmeister <49274952+Murmelmeister@users.noreply.github.com>
2026-01-18 12:35:18 +0100

    refactor: integrate `RefreshProvider` into `InventoryTypeProviderImpl`, enhance null-safety

--- src/main/java/de/murmelmeister/murmelapi/inventory/InventoryTypeProviderImpl.java
+++ src/main/java/de/murmelmeister/murmelapi/inventory/InventoryTypeProviderImpl.java
@@ -2,8 +2,11 @@ package de.murmelmeister.murmelapi.inventory;
 
 import de.murmelmeister.library.database.Database;
 import de.murmelmeister.library.utils.StringUtil;
+import de.murmelmeister.murmelapi.utils.update.RefreshProvider;
 import de.murmelmeister.murmelapi.utils.update.RefreshType;
-import de.murmelmeister.murmelapi.utils.update.RefreshUtil;
+import org.intellij.lang.annotations.Language;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.time.Duration;
 import java.util.List;
@@ -12,42 +15,45 @@ public final class InventoryTypeProviderImpl implements InventoryTypeProvider {
     private static final String TABLE_NAME = "inventory_type";
 
     private final Database database;
+    private final RefreshProvider refreshProvider;
     private final InventoryTypeCache cache;
     private final RefreshType all = RefreshType.INVENTORY_TYPES;
     private final RefreshType single = RefreshType.SINGLE_INVENTORY_TYPE;
 
-    public InventoryTypeProviderImpl(Database database, Long fetchLimit, long cacheCapacity, Duration refreshInterval) {
+    public InventoryTypeProviderImpl(Database database, RefreshProvider refreshProvider, Long fetchLimit, long cacheCapacity, Duration refreshInterval) {
         this.database = database;
-        this.cache = new InventoryTypeCache(database, TABLE_NAME, fetchLimit, cacheCapacity, refreshInterval);
+        this.refreshProvider = refreshProvider;
+        this.cache = new InventoryTypeCache(database, refreshProvider, TABLE_NAME, fetchLimit, cacheCapacity, refreshInterval);
     }
 
     @Override
     public void refreshCache() {
-        RefreshUtil.fireCache(all);
+        refreshProvider.fireCache(all);
     }
 
     @Override
-    public InventoryType findById(int id) {
+    public @Nullable InventoryType findById(int id) {
         return cache.getById(id);
     }
 
     @Override
-    public List<InventoryType> findAll() {
+    public @NotNull List<InventoryType> findAll() {
         return cache.getAll();
     }
 
     @Override
-    public InventoryType create(String name) {
+    public @Nullable InventoryType create(@NotNull String name) {
         String normalizedName = StringUtil.normalize(name);
         if (normalizedName == null)
             return null;
 
-        String sql = "INSERT INTO " + TABLE_NAME + " (inventory_name) VALUES (?)";
+        @Language("MariaDB")
+        String sql = "INSERT INTO %s (inventory_name) VALUES (?)".formatted(TABLE_NAME);
         int id = (int) database.updateAndGetGeneratedKeys(sql, stmt -> stmt.setString(1, normalizedName));
         if (id < 1) return null;
 
         InventoryType type = new InventoryType(id, normalizedName);
-        RefreshUtil.fireSingle(single, type.id());
+        refreshProvider.fireSingle(single, type.id());
         return type;
     }
 
@@ -55,11 +61,12 @@ public final class InventoryTypeProviderImpl implements InventoryTypeProvider {
     public int delete(int id) {
         if (id < 1) return 0;
 
-        String sql = "DELETE FROM " + TABLE_NAME + " WHERE id = ?";
+        @Language("MariaDB")
+        String sql = "DELETE FROM %s WHERE id = ?".formatted(TABLE_NAME);
         int row = database.update(sql, stmt -> stmt.setInt(1, id));
         if (row < 1) return 0;
 
-        RefreshUtil.fireSingle(single, id);
+        refreshProvider.fireSingle(single, id);
         return row;
     }
 }
