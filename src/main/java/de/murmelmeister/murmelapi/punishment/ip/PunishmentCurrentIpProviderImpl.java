5f4283ed6beecefa3e7978b7ba7af4355b257917
Murmelmeister <49274952+Murmelmeister@users.noreply.github.com>
2026-01-18 12:37:04 +0100

    refactor: integrate `RefreshProvider`, enhance null-safety annotations

--- src/main/java/de/murmelmeister/murmelapi/punishment/ip/PunishmentCurrentIpProviderImpl.java
+++ src/main/java/de/murmelmeister/murmelapi/punishment/ip/PunishmentCurrentIpProviderImpl.java
@@ -1,8 +1,12 @@
 package de.murmelmeister.murmelapi.punishment.ip;
 
 import de.murmelmeister.library.database.Database;
+import de.murmelmeister.murmelapi.utils.update.RefreshProvider;
 import de.murmelmeister.murmelapi.utils.update.RefreshType;
-import de.murmelmeister.murmelapi.utils.update.RefreshUtil;
+import org.intellij.lang.annotations.Language;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.Unmodifiable;
 
 import java.net.InetAddress;
 import java.time.Duration;
@@ -14,22 +18,24 @@ public final class PunishmentCurrentIpProviderImpl implements PunishmentCurrentI
     public static final String TABLE_NAME = "punishment_current_ip";
 
     private final Database database;
+    private final RefreshProvider refreshProvider;
     private final PunishmentCurrentIpCache cache;
     private final RefreshType all = RefreshType.PUNISHMENT_IPS;
     private final RefreshType single = RefreshType.SINGLE_PUNISHMENT_IP;
 
-    public PunishmentCurrentIpProviderImpl(Database database, Long fetchLimit, long cacheCapacity, Duration refreshInterval) {
+    public PunishmentCurrentIpProviderImpl(Database database, RefreshProvider refreshProvider, Long fetchLimit, long cacheCapacity, Duration refreshInterval) {
         this.database = database;
-        this.cache = new PunishmentCurrentIpCache(database, TABLE_NAME, fetchLimit, cacheCapacity, refreshInterval);
+        this.refreshProvider = refreshProvider;
+        this.cache = new PunishmentCurrentIpCache(database, refreshProvider, TABLE_NAME, fetchLimit, cacheCapacity, refreshInterval);
     }
 
     @Override
     public void refreshCache() {
-        RefreshUtil.fireCache(all);
+        refreshProvider.fireCache(all);
     }
 
     @Override
-    public List<InetAddress> getAllPunishedIps(int typeId) {
+    public @NotNull @Unmodifiable List<InetAddress> getAllPunishedIps(int typeId) {
         return cache.getCachedPunishIPs().stream()
                 .filter(punish -> punish.typeId() == typeId)
                 .map(PunishmentCurrentIp::inetAddress)
@@ -37,20 +43,20 @@ public final class PunishmentCurrentIpProviderImpl implements PunishmentCurrentI
     }
 
     @Override
-    public PunishmentCurrentIp getPunishedIp(InetAddress inetAddress, int typeId) {
+    public @Nullable PunishmentCurrentIp getPunishedIp(@NotNull InetAddress inetAddress, int typeId) {
         return cache.get(inetAddress, typeId);
     }
 
     @Override
-    public PunishmentCurrentIp create(InetAddress inetAddress, int typeId, UUID logId) {
-        if (inetAddress == null || typeId < 1 || logId == null)
+    public @Nullable PunishmentCurrentIp create(@NotNull InetAddress inetAddress, int typeId, @NotNull UUID logId) {
+        if (typeId < 1)
             return null;
 
-        //ipAddress = ipAddress.strip();
-        //if (ipAddress.isEmpty()) return null;
-
-        String sql = "INSERT INTO " + TABLE_NAME + " (ip_address, type_id, log_id) VALUES (?, ?, ?)";
-        //String finalIpAddress = ipAddress;
+        @Language("MariaDB")
+        String sql = """
+                INSERT INTO %s (ip_address, type_id, log_id)
+                VALUES (?, ?, ?)
+                """.formatted(TABLE_NAME);
         int row = database.update(sql, stmt -> {
             stmt.setString(1, inetAddress.getHostAddress());
             stmt.setInt(2, typeId);
@@ -59,34 +65,30 @@ public final class PunishmentCurrentIpProviderImpl implements PunishmentCurrentI
         if (row < 1) return null;
 
         PunishmentCurrentIp punish = new PunishmentCurrentIp(inetAddress, typeId, logId);
-        RefreshUtil.fireSingle(single, new PunishmentCurrentIpCache.IpTypeKey(inetAddress, typeId));
+        refreshProvider.fireSingle(single, new PunishmentCurrentIpCache.IpTypeKey(inetAddress, typeId));
         return punish;
     }
 
     @Override
-    public int delete(InetAddress inetAddress, int typeId) {
-        if (inetAddress == null || typeId < 1)
+    public int delete(@NotNull InetAddress inetAddress, int typeId) {
+        if (typeId < 1)
             return 0;
 
-        //ipAddress = ipAddress.strip();
-        //if (ipAddress.isEmpty()) return 0;
-
-        String sql = "DELETE FROM " + TABLE_NAME + " WHERE ip_address = ? AND type_id = ?";
-        //String finalIpAddress = ipAddress;
+        @Language("MariaDB")
+        String sql = "DELETE FROM %s WHERE ip_address = ? AND type_id = ?".formatted(TABLE_NAME);
         int row = database.update(sql, stmt -> {
             stmt.setString(1, inetAddress.getHostAddress());
             stmt.setInt(2, typeId);
         });
         if (row < 1) return 0;
 
-        RefreshUtil.fireSingle(single, new PunishmentCurrentIpCache.IpTypeKey(inetAddress, typeId));
+        refreshProvider.fireSingle(single, new PunishmentCurrentIpCache.IpTypeKey(inetAddress, typeId));
         return row;
     }
 
     @Override
-    public PunishmentCurrentIp update(InetAddress inetAddress, int typeId, UUID logId) {
-        //String normalizedIpAddress = StringUtil.normalize(ipAddress);
-        if (inetAddress == null || typeId < 1 || logId == null)
+    public PunishmentCurrentIp update(@NotNull InetAddress inetAddress, int typeId, @NotNull UUID logId) {
+        if (typeId < 1)
             return null;
 
         PunishmentCurrentIp existing = cache.get(inetAddress, typeId);
@@ -95,7 +97,8 @@ public final class PunishmentCurrentIpProviderImpl implements PunishmentCurrentI
         if (Objects.equals(logId, existing.logId()))
             return existing; // No update needed if the log ID is the same
 
-        String sql = "UPDATE " + TABLE_NAME + " SET log_id = ? WHERE ip_address = ? AND type_id = ?";
+        @Language("MariaDB")
+        String sql = "UPDATE %s SET log_id = ? WHERE ip_address = ? AND type_id = ?".formatted(TABLE_NAME);
         int row = database.update(sql, stmt -> {
             stmt.setString(1, logId.toString());
             stmt.setString(2, inetAddress.getHostAddress());
@@ -104,7 +107,7 @@ public final class PunishmentCurrentIpProviderImpl implements PunishmentCurrentI
         if (row < 1) return null;
 
         PunishmentCurrentIp punish = existing.withUpdateLog(logId);
-        RefreshUtil.fireSingle(single, new PunishmentCurrentIpCache.IpTypeKey(inetAddress, typeId));
+        refreshProvider.fireSingle(single, new PunishmentCurrentIpCache.IpTypeKey(inetAddress, typeId));
         return punish;
     }
 }
