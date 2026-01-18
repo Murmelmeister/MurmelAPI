2cde4362edc21c4e9a1470537dce58c1acf952a5
Murmelmeister <49274952+Murmelmeister@users.noreply.github.com>
2026-01-18 12:37:23 +0100

    refactor: enhance null-safety annotations in `PunishmentReasonProvider` methods

--- src/main/java/de/murmelmeister/murmelapi/punishment/reason/PunishmentReasonProvider.java
+++ src/main/java/de/murmelmeister/murmelapi/punishment/reason/PunishmentReasonProvider.java
@@ -1,19 +1,22 @@
 package de.murmelmeister.murmelapi.punishment.reason;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
 import java.util.List;
 
 public interface PunishmentReasonProvider {
     void refreshCache();
 
-    PunishmentReason getReason(int id);
+    @Nullable PunishmentReason getReason(int id);
 
-    List<PunishmentReason> getReasonsByType(int typeId);
+    @Nullable List<PunishmentReason> getReasonsByType(int typeId);
 
-    List<PunishmentReason> getAllReasons();
+    @NotNull List<PunishmentReason> getAllReasons();
 
-    PunishmentReason create(int id, int typeId, String reasonText, Long durationSecs, boolean autoFlagIp, boolean autoPunish, int createdBy);
+    @Nullable PunishmentReason create(int id, int typeId, @NotNull String reasonText, @Nullable Long durationSecs, boolean autoFlagIp, boolean autoPunish, int createdBy);
 
     int delete(int id);
 
-    PunishmentReason update(int id, int typeId, String reasonText, Long durationSecs, boolean autoFlagIp, boolean autoPunish, int changedBy);
+    @Nullable PunishmentReason update(int id, int typeId, @NotNull String reasonText, @Nullable Long durationSecs, boolean autoFlagIp, boolean autoPunish, int changedBy);
 }
