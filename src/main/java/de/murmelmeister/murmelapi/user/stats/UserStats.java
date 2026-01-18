dd617340b655bbeb46c71fb2126e1318abe91e0c
Murmelmeister <49274952+Murmelmeister@users.noreply.github.com>
2026-01-18 13:56:15 +0100

    refactor: add `UserStats.Builder` for mutable operations and enforce null-safety annotations

--- src/main/java/de/murmelmeister/murmelapi/user/stats/UserStats.java
+++ src/main/java/de/murmelmeister/murmelapi/user/stats/UserStats.java
@@ -1,8 +1,60 @@
 package de.murmelmeister.murmelapi.user.stats;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-public record UserStats(int userId, int playTime, int dailyStreak, LocalDate dailyStreakLastDay,
-                        LocalDateTime lastSeenAt) {
+public record UserStats(
+        int userId,
+        int playTime,
+        int dailyStreak,
+        @Nullable LocalDate dailyStreakLastDay,
+        @Nullable LocalDateTime lastSeenAt
+) {
+    public static @NotNull Builder builder(@NotNull UserStats userStats) {
+        return new Builder(userStats);
+    }
+
+    public static class Builder {
+        private final int userId;
+
+        private int playTime;
+        private int dailyStreak;
+        private LocalDate dailyStreakLastDay;
+        private LocalDateTime lastSeenAt;
+
+        private Builder(@NotNull UserStats userStats) {
+            this.userId = userStats.userId();
+            this.playTime = userStats.playTime();
+            this.dailyStreak = userStats.dailyStreak();
+            this.dailyStreakLastDay = userStats.dailyStreakLastDay();
+            this.lastSeenAt = userStats.lastSeenAt();
+        }
+
+        public Builder playTime(int playTime) {
+            this.playTime = playTime;
+            return this;
+        }
+
+        public Builder dailyStreak(int dailyStreak) {
+            this.dailyStreak = dailyStreak;
+            return this;
+        }
+
+        public Builder dailyStreakLastDay(@Nullable LocalDate dailyStreakLastDay) {
+            this.dailyStreakLastDay = dailyStreakLastDay;
+            return this;
+        }
+
+        public Builder lastSeenAt(@Nullable LocalDateTime lastSeenAt) {
+            this.lastSeenAt = lastSeenAt;
+            return this;
+        }
+
+        public @NotNull UserStats build() {
+            return new UserStats(userId, playTime, dailyStreak, dailyStreakLastDay, lastSeenAt);
+        }
+    }
 }
