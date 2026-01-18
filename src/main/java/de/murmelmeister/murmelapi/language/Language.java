d7a35b5cdf721ebf3c6a836325fe0a0bb97b9bf7
Murmelmeister <49274952+Murmelmeister@users.noreply.github.com>
2026-01-18 12:35:23 +0100

    refactor: enforce null-safety for `Language` record constructor and methods

--- src/main/java/de/murmelmeister/murmelapi/language/Language.java
+++ src/main/java/de/murmelmeister/murmelapi/language/Language.java
@@ -1,11 +1,13 @@
 package de.murmelmeister.murmelapi.language;
 
+import org.jetbrains.annotations.NotNull;
+
 /**
  * Represents a language with an ID and a language code, stored as a BCP 47 string.
  * This class is used to manage different languages in the application.
  */
-public record Language(int id, String code) {
-    public Language withCode(String code) {
+public record Language(int id, @NotNull String code) {
+    public @NotNull Language withCode(String code) {
         return new Language(id, code != null ? code : this.code);
     }
 }
