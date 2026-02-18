40d66fbf0d69545fe1c6b5e0344c0c1dbd27c53e
Murmelmeister <49274952+Murmelmeister@users.noreply.github.com>
2026-02-18 21:21:04 +0100

    refactor: add null-safety in JSON parsing and logging

--- src/main/java/de/murmelmeister/murmelapi/group/permission/GroupPermissionProviderImpl.java
+++ src/main/java/de/murmelmeister/murmelapi/group/permission/GroupPermissionProviderImpl.java
@@ -1,5 +1,6 @@
 package de.murmelmeister.murmelapi.group.permission;
 
+import com.google.gson.Gson;
 import de.murmelmeister.library.database.Database;
 import de.murmelmeister.library.utils.StringUtil;
 import de.murmelmeister.murmelapi.utils.update.RefreshProvider;
@@ -29,10 +30,10 @@ public final class GroupPermissionProviderImpl implements GroupPermissionProvide
     private final RefreshType all = RefreshType.GROUP_PERMISSIONS;
     private final RefreshType single = RefreshType.SINGLE_GROUP_PERMISSION;
 
-    public GroupPermissionProviderImpl(Database database, RefreshProvider refreshProvider, Long fetchLimit, long cacheCapacity, Duration refreshInterval) {
+    public GroupPermissionProviderImpl(Database database, Gson gson, RefreshProvider refreshProvider, Long fetchLimit, long cacheCapacity, Duration refreshInterval) {
         this.database = database;
         this.refreshProvider = refreshProvider;
-        this.cache = new GroupPermissionCache(database, refreshProvider, TABLE_NAME, fetchLimit, cacheCapacity, refreshInterval);
+        this.cache = new GroupPermissionCache(database, gson, refreshProvider, TABLE_NAME, fetchLimit, cacheCapacity, refreshInterval);
     }
 
     @Override
