5c0c13e36d7de7fdc87638bf6f09811a222f83bc
Murmelmeister <49274952+Murmelmeister@users.noreply.github.com>
2026-02-19 21:28:28 +0100

    refactor: add `MaintenanceCache` and `MaintenanceProvider` implementation

---
+++ src/main/java/de/murmelmeister/murmelapi/maintenance/MaintenanceProviderImpl.java
@@ -0,0 +1,24 @@
+package de.murmelmeister.murmelapi.maintenance;
+
+import com.google.gson.Gson;
+import de.murmelmeister.library.database.Database;
+import de.murmelmeister.murmelapi.utils.update.RefreshProvider;
+import de.murmelmeister.murmelapi.utils.update.RefreshType;
+
+import java.time.Duration;
+
+public final class MaintenanceProviderImpl implements MaintenanceProvider {
+    private static final String TABLE_NAME = "maintenance_windows";
+
+    private final Database database;
+    private final RefreshProvider refreshProvider;
+    private final MaintenanceCache cache;
+    private final RefreshType all = RefreshType.MAINTENANCES;
+    private final RefreshType single = RefreshType.SINGLE_MAINTENANCE;
+
+    public MaintenanceProviderImpl(Database database, Gson gson, RefreshProvider refreshProvider, Long fetchLimit, long cacheCapacity, Duration refreshInterval) {
+        this.database = database;
+        this.refreshProvider = refreshProvider;
+        this.cache = new MaintenanceCache(database, gson, refreshProvider, TABLE_NAME, fetchLimit, cacheCapacity, refreshInterval);
+    }
+}
